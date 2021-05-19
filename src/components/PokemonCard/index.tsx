6e3bbf3ba7639875fc27457bd54146f05bba8515
Konstantin Korostelev <korostel97@gmail.com>
2021-05-19 17:57:59 +0300

    fix PokemonCard and PokedexPage

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import cn from 'classnames';
 import Heading from '../Heading';
 
 import s from './PokemonCard.module.scss';
@@ -15,46 +14,29 @@ interface PokemonCardProps {
   img: string;
 }
 
-const PokemonCard: React.FC<PokemonCardProps> = ({ name, types, stats, img }) => {
+const PokemonCard: React.FC<PokemonCardProps> = ({ name, stats, types, img }) => {
   return (
     <div className={s.root}>
-      <div
-        className={cn(s.img, {
-          [s.fire]: types[0] === 'fire',
-          [s.grass]: types[0] === 'grass',
-          [s.blaze]: types[0] === 'blaze',
-          [s.water]: types[0] === 'water',
-          [s.bug]: types[0] === 'bug',
-        })}>
-        <img src={img} alt={`${name}_logo`} />
+      <div className={s.infoWrap}>
+        <Heading type="h4" className={s.titleName}>
+          {name}
+        </Heading>
+        <div className={s.statWrap}>
+          <div className={s.statItem}>
+            <div className={s.statValue}>{stats.attack}</div>
+            Attack
+          </div>
+          <div className={s.statItem}>
+            <div className={s.statValue}>{stats.defense}</div>
+            Defense
+          </div>
+        </div>
+        <div className={s.labelWrap}>
+          <span className={s.label}>{types[0]}</span>
+        </div>
       </div>
-      <div className={s.pokemonData}>
-        <Heading type="h4">{name}</Heading>
-        <div className={s.stats}>
-          <div>
-            <div className={s.statCircle}>{stats.attack}</div>
-            <div className={s.statName}>Attack</div>
-          </div>
-          <div>
-            <div className={s.statCircle}>{stats.defense}</div>
-            <div className={s.statName}>Defense</div>
-          </div>
-        </div>
-        <div className={s.types}>
-          {types.map((item) => (
-            <div
-              className={cn(s.type, {
-                [s.grass]: item === 'grass',
-                [s.poison]: item === 'poison',
-                [s.fire]: item === 'fire',
-                [s.water]: item === 'water',
-                [s.bug]: item === 'bug',
-                [s.flying]: item === 'flying',
-              })}>
-              {item}
-            </div>
-          ))}
-        </div>
+      <div className={s.pictureWrap}>
+        <img src={img} alt={name} />
       </div>
     </div>
   );
